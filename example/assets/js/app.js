24282869e3f842c98408a5e9a93f727966bde59f
Gabe Hernandez <ghernandez345@gmail.com>
2014-03-10 18:01:17 -0500

    added docs

--- example/assets/js/app.js
+++ example/assets/js/app.js
@@ -7,6 +7,10 @@
     // Get the comments from the sails server.
     $scope.comments = $sails('/comment');
 
+    // You can also add a second query paramater to get resources at endpoint that fulfull
+    // the criteria.
+    // $scope.comments = $sails('/comment', {body: 'test'});
+
     // Adds a comment.
     $scope.addComment = function (e) {
       if (e.keyCode !== 13) return;
