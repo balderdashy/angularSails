5c73a2a1e853e6507137ec034a60e0269cdca2ea
Rob Wormald <rob.wormald@innitapps.com>
2014-08-23 00:06:27 -0500

    trying to get ptor and codeship working :(

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
   baseUrl:'http://127.0.0.1:1337',
   // Spec patterns are relative to the current working directly when
   // protractor is called.
-  specs: ['test/protractor/protractor.js'],
+  specs: ['test/e2e/**.*.js'],
  
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
