5621deb10e650b93e5112e6304ac80d81ec01aa9
Rob Wormald <rob.wormald@innitapps.com>
2014-08-23 00:32:23 -0500

    working locally. codeship?

--- test/e2e/lifecycle.test.js
+++ test/e2e/lifecycle.test.js
@@ -1,11 +1,16 @@
-beforeEach(function() {
+describe('angularSails E2E',function(){
+
+	beforeEach(function() {
   browser.get('http://127.0.0.1:1337/');
 });
 
 it('should find title element', function() {
   browser.get('index.html');
 
-  browser.debugger();
+  //browser.debugger();
 
-  element(by.binding('user.name'));
+  	expect(browser.getTitle()).toEqual('angularSails');
 });
+
+
+})
