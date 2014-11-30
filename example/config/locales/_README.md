9d2b1fcb7cdddf4b9a276579e781dfe2f8709177
Rob Wormald <rob@jiff.com>
2014-11-29 19:29:48 -0800

    Revert "example reds"
    This reverts commit 27eaed8bcfa601f2351066b4e0324beec33475b3.

--- example/config/locales/_README.md
+++ example/config/locales/_README.md
@@ -1,13 +1,9 @@
 # Internationalization / Localization Settings
 
-> Also see the official docs on internationalization/localization:
-> http://links.sailsjs.org/docs/config/locales
+## Locale
+All locale files live under `config/locales`. Here is where you can add locale data as JSON key-value pairs. The name of the file should match the language that you are supporting, which allows for automatic language detection based on the user request.
 
-## Locales
-All locale files live under `config/locales`. Here is where you can add translations
-as JSON key-value pairs. The name of the file should match the language that you are supporting, which allows for automatic language detection based on request headers.
-
-Here is an example locale stringfile for the Spanish language (`config/locales/es.json`):
+Here is an example locale stringfile for the Spanish language (`config/locales/es.json`):  
 ```json
 {
     "Hello!": "Hola!",
--- example/config/log.js
+++ example/config/log.js
@@ -1,29 +1,27 @@
 /**
- * Built-in Log Configuration
- * (sails.config.log)
+ * Logger configuration
  *
  * Configure the log level for your app, as well as the transport
- * (Underneath the covers, Sails uses Winston for logging, which
+ * (Underneath the covers, Sails uses Winston for logging, which 
  * allows for some pretty neat custom transports/adapters for log messages)
  *
  * For more information on the Sails logger, check out:
- * http://sailsjs.org/#/documentation/concepts/Logging
+ * http://sailsjs.org/#documentation
  */
 
-module.exports.log = {
+module.exports = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Valid `level` configs: i.e. the minimum log level to capture with        *
-  * sails.log.*()                                                            *
-  *                                                                          *
-  * The order of precedence for log levels from lowest to highest is:        *
-  * silly, verbose, info, debug, warn, error                                 *
-  *                                                                          *
-  * You may also set the level to "silent" to suppress all logs.             *
-  *                                                                          *
-  ***************************************************************************/
-
-  // level: 'info'
+  // Valid `level` configs:
+  // i.e. the minimum log level to capture with sails.log.*()
+  //
+  // 'error'	: Display calls to `.error()`
+  // 'warn'	: Display calls from `.error()` to `.warn()`
+  // 'debug'	: Display calls from `.error()`, `.warn()` to `.debug()`
+  // 'info'	: Display calls from `.error()`, `.warn()`, `.debug()` to `.info()`
+  // 'verbose': Display calls from `.error()`, `.warn()`, `.debug()`, `.info()` to `.verbose()`
+  //
+  log: {
+    level: 'silly'
+  }
 
 };
