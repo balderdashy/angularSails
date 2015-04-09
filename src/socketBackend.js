7753fa049ac898fc1e09cde739aaf929deb5fe58
Rob Wormald <robwormald@gmail.com>
2015-04-09 16:10:21 -0700

    re-add header fix

--- src/socketBackend.js
+++ src/socketBackend.js
@@ -42,7 +42,12 @@ function createSailsBackend($browser, $window, $injector, $q, $timeout){
         url = url || $browser.url();
 
 
-        $window.io.socket[method.toLowerCase()](url,fromJson(post),socketResponse);
+        $window.io.socket._request({
+            method: method.toLowerCase(),
+            url: url,
+            data: fromJson(post),
+            headers: headers
+        }, socketResponse);
 
     }
 
@@ -78,4 +83,3 @@ function sailsBackendProvider() {
         return createSailsBackend($browser,$window, $injector, $q,$timeout);
     }];
 }
-
